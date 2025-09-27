c1a15d83a1b9fab45798e600554e753fc18aaa6b
Samiraid1993 <samiraid.electronique@gmail.com>
2025-09-27 18:08:03 +0200

    Delete src directory

--- src/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import HRManagementTool from './HRManagementTool'
-
-export default function App() {
-  return <HRManagementTool />
-}
--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://zcbzlczupnwsdfngolca.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjYnpsY3p1cG53c2RmbmdvbGNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5NzkzOTEsImV4cCI6MjA3NDU1NTM5MX0.WtEDMB9buLbo0YZqyxBWNRPNoTqhwMpMiGpJ_K_ihy4";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
